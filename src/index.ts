5e97ca4473c3d6feb1be72d3bbb3c8d67fd65197
Fahadul Islam <44726129+DaPotatoMan@users.noreply.github.com>
2022-02-09 12:25:27 +0600

    feat: update dist

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+// ((proxied) => {
+//   navigator.mediaDevices.getUserMedia = function() {
+//     console.log('someone accessing user media')
+
+//     // @ts-expect-error type issue
+//     // eslint-disable-next-line prefer-rest-params
+//     return proxied.apply(this, arguments)
+//   }
+// })(navigator.mediaDevices.getUserMedia)
+
 export { default } from './recorder'
